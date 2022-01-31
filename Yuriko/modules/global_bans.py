8f8e6c0482931fbf6f1cd2590c06e23c3c749e0b
丂ﾑﾶﾶƳ <89745675+FriDayXD@users.noreply.github.com>
2022-01-31 21:02:26 +0530

    Update global_bans.py

--- Yuriko/modules/global_bans.py
+++ Yuriko/modules/global_bans.py
@@ -540,7 +540,7 @@ def __chat_settings__(chat_id, user_id):
 __help__ = f"""
 *Admins only:*
 
-乂 /antispam - `<on/off/yes/no> Will toggle our antispam tech or return your current settings.`
+/antispam - `<on/off/yes/no> Will toggle our antispam tech or return your current settings.`
 
 `Anti-Spam used by bot devs to ban spammers across all groups. This helps protect`
 `you and your groups by removing spam flooders as quickly as possible.`
@@ -568,7 +568,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "Anti-Spam"
+__mod_name__ = "AntiSpam"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set

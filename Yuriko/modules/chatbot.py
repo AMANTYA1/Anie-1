cb497071149d58f24f2615597465a4ba8fa52a28
丂ﾑﾶﾶƳ <89745675+FriDayXD@users.noreply.github.com>
2022-01-31 21:05:23 +0530

    Update chatbot.py

--- Yuriko/modules/chatbot.py
+++ Yuriko/modules/chatbot.py
@@ -138,16 +138,14 @@ def list_all_chats(update: Update, context: CallbackContext):
     update.effective_message.reply_text(text, parse_mode="HTML")
 
 __help__ = """
- `Chatbot utilizes the` *YURIKO* `api which allows Yuriko to talk and provide a more interactive group chat experience.`
+ `Chatbot utilizes the` *AnieBot* `api which allows AnieBot to talk and provide a more interactive group chat experience.`
 
 *Admins only Commands*:
  
  /Chatbot - `Shows chatbot control panel`
-  
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: TGN!*
-"""
+ """ 
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "ChatBot"
 
 
 CHATBOTK_HANDLER = CommandHandler("chatbot", kuki)
